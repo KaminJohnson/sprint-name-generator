b490317cbbb600259850f1828ea8046060f278e0
Gabe Landau <me@gabelandau.com>
2023-10-12 22:11:43 -0400

    feat: add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { Analytics } from '@vercel/analytics/react';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="bg-slate-950 text-slate-50">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
